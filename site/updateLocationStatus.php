b20ffd331e47796d8fad65678735c6cbeb2e0859
SeanNguyen <firestormkjc.1993@gmail.com>
2014-10-01 15:20:01 +0800

    Fixed error in update status of location

--- site/updateLocationStatus.php
+++ site/updateLocationStatus.php
@@ -2,10 +2,6 @@
 // configuration
 require('includes/config.php'); 
 
-// new data
-$title = 'PHP Security';
-$author = 'Jack Hijack';
-
 $timeOut = intval($_GET['timeOut']);
 
 $status = 0;
@@ -14,11 +10,15 @@ if ($timeOut > 0)
 
 $username = $_SESSION['username'];
 
-$timeOut = '"' + "$timeOut" + '"';
-$status = '"' + "$status" + '"';
-$username = '"' + "$username" + '"';
+echo $username;
+
+
+$timeOut = '"'.$timeOut.'"';
+$status = '"'.$status.'"';
+$username = '"'.$username.'"';
 
 // query
 $sql = "UPDATE members SET status = $status, requestTimeout = $timeOut WHERE username = $username;";
+echo $sql;
 $q = $db->query($sql);
 ?>
